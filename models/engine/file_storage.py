af51eb445a4357f20ef1fbece73f82f2c2fa96c7
Tycee02 <wakilaminat@gmail.com>
2024-03-20 12:54:22 +0100

    Delete object

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+import shlex
 
 
 class FileStorage:
-    """Returns a dictionary"""
+    """This class serializes instances to a JSON file
+    and deserializes JSON file to instances
+    """
     __file_path = 'file.json'
     __objects = {}
 
