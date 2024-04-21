98a8ac811e25c81d46b81b813d2fc6935f134dd7
Tycee02 <wakilaminat@gmail.com>
2024-04-21 10:32:32 +0100

    Hello Flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,9 +6,11 @@ Start a flask web application
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def index():
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
